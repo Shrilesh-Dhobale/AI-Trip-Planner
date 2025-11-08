75308d30546ce4bd6adab6610a12b36684f295cd
dshrileshdhobale@gmail.com <dshrilesh@gmail.com>
2025-11-08 20:36:13 +0530

    Hero component created

---
+++ src/components/custom/Hero.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Hero() {
+  return (
+    <div>
+      <h1 className="text-4xl font-bold">Plan Your Trip</h1>
+      <p className="mt-2 text-lg">Discover amazing places and experiences</p>
+    </div>
+  )
+}
+
+export default Hero
