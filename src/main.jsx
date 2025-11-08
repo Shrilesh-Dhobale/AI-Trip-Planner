fcf58d961a87ce0f5f32829ecd503b5ef0fbf5f1
dshrileshdhobale@gmail.com <dshrilesh@gmail.com>
2025-11-08 18:45:30 +0530

    CreateTrip component imported

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import CreateTrip from '@/create-trip/index.jsx'
 
 const router=createBrowserRouter([
   {
