f32f6201df216e48feee9e09f8ba659e52c0eb59
dshrileshdhobale@gmail.com <dshrilesh@gmail.com>
2025-11-08 20:21:47 +0530

    Tailwind applied successfuly

--- src/components/custom/Header.jsx
+++ src/components/custom/Header.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
+import { Button } from '@/components/ui/button'
 
 function Header() {
   return (
-    <div>
-      
+    <div className='p-2 shadow-md flex justify-between items-center bg-red-200'>
+      <img src="/logo.png" alt="Logo" className="h-4 w-4" />
+      <div>
+        <Button/>
+      </div>
     </div>
   )
 }
