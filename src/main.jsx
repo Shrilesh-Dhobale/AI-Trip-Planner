adf840038e28503f89c0e08d112d26153b05fbea
dshrileshdhobale@gmail.com <dshrilesh@gmail.com>
2025-11-08 18:44:20 +0530

    path created for CreateTrip component

--- src/main.jsx
+++ src/main.jsx
@@ -9,6 +9,10 @@ const router=createBrowserRouter([
     path: '/',
     element: <App/>,
   },
+  {
+    path: '/create-trip',
+    element: <CreateTrip/>,
+  }
 ])
   
 createRoot(document.getElementById('root')).render(
