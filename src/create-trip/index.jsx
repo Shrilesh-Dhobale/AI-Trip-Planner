650c867cf03d617937c05e80e347e3ab359120f7
dshrileshdhobale@gmail.com <dshrilesh@gmail.com>
2025-11-09 19:24:30 +0530

    var created for props

--- src/create-trip/index.jsx
+++ src/create-trip/index.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import GooglePlacesAutocomplete from 'react-google-places-autocomplete';
 
 function CreateTrip() {
+  const [place,setplace]=useState();
   return (
     <div className='sm:px-10 md:px-32 lg:px-56 xl:px-72 px-5 mt-10'>
       <h2 className='font-bold text-3xl'>Tell us your travel preferences</h2>
