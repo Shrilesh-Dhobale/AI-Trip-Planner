879d1466af8bd6f21ce1c9e234198b8aa854dd18
dshrileshdhobale@gmail.com <dshrilesh@gmail.com>
2025-11-09 17:02:23 +0530

    Sub title added

--- src/components/custom/Header.jsx
+++ src/components/custom/Header.jsx
@@ -3,7 +3,7 @@ import { Button } from '@/components/ui/button'
 
 function Header() {
   return (
-    <div className=' shadow-md flex justify-between items-center'>
+    <div className=' shadow-sm flex justify-between items-center'>
       <img src="/logo.png" alt="Logo" className="h-25 w-25" />
       <div>
         <Button/>
--- src/components/custom/Hero.jsx
+++ src/components/custom/Hero.jsx
@@ -2,9 +2,12 @@ import React from 'react'
 
 function Hero() {
   return (
-    <div>
-      <h1 className="text-4xl font-bold">Plan Your Trip</h1>
-      <p className="mt-2 text-lg">Discover amazing places and experiences</p>
+    <div className='flex items-center mx-56 gap-9'>
+      <h1 className="font-extrabold text-[47px] text-center mt-16">
+        <span className='text-[#1E245B]'>Discover Your Perfect Trip with AI:</span><br />
+        Personalized Itineraries at Your Fingertips
+      </h1>
+      
     </div>
   )
 }
