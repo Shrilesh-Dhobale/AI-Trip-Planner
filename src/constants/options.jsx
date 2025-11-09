12a347be4525e8fa24327a83596fc112a1cbd143
dshrileshdhobale@gmail.com <dshrilesh@gmail.com>
2025-11-09 19:44:56 +0530

    Info added for 1

---
+++ src/constants/options.jsx
@@ -0,0 +1,11 @@
+export const SelectTravelList = [
+    {
+        id:1,
+        title:"Just me",
+        desc:"Solo trip tailored to your interests"
+        icon:"✈︎",
+        people:'1'
+    },
+    
+        
+    
