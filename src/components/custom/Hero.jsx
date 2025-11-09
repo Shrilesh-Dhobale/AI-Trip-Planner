c292d1922ac5d6399b83115e71e3a3623e644309
dshrileshdhobale@gmail.com <dshrilesh@gmail.com>
2025-11-09 17:08:14 +0530

    Align in col

--- src/components/custom/Hero.jsx
+++ src/components/custom/Hero.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Hero() {
   return (
-    <div className='flex items-center mx-56 gap-9'>
+    <div className='flex flex-col items-center  mx-56 gap-9'>
       <h1 className="font-extrabold text-[47px] text-center mt-16">
         <span className='text-[#1E245B]'>Discover Your Perfect Trip with AI:</span><br />
         Personalized Itineraries at Your Fingertips
