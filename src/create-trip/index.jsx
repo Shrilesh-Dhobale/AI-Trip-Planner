a0fa59e6044e39dc6e115ed4a9abcd64b17e21a7
dshrileshdhobale@gmail.com <dshrilesh@gmail.com>
2025-11-10 20:24:39 +0530

    Var created for form data

--- src/create-trip/index.jsx
+++ src/create-trip/index.jsx
@@ -7,6 +7,9 @@ import { Button } from '@/components/ui/button';
 
 function CreateTrip() {
   const [place,setplace]=useState();
+
+  const [formData,setFormData]=useState([]);
+
   return (
     <div className='sm:px-10 md:px-32 lg:px-56 xl:px-72 px-5 mt-10'>
       <h2 className='font-bold text-3xl'>Tell us your travel preferences🏕️🏖️</h2>
