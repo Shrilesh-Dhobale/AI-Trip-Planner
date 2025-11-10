c2d81df568256b946766ae06f998af5157818fcb
dshrileshdhobale@gmail.com <dshrilesh@gmail.com>
2025-11-10 20:22:39 +0530

    Emoji added in heading

--- src/create-trip/index.jsx
+++ src/create-trip/index.jsx
@@ -9,7 +9,7 @@ function CreateTrip() {
   const [place,setplace]=useState();
   return (
     <div className='sm:px-10 md:px-32 lg:px-56 xl:px-72 px-5 mt-10'>
-      <h2 className='font-bold text-3xl'>Tell us your travel preferences</h2>
+      <h2 className='font-bold text-3xl'>Tell us your travel preferences🏕️🏖️</h2>
       <p className='mt-3 text-gray-500 text-xl'>Just provide some basic information, and our trip planner</p>
       
       <div className='mt-20 flex flex-col gap-9'>
