87c8dd5e80cecf169f2d77e703bf10bccc8c4d40
dshrileshdhobale@gmail.com <dshrilesh@gmail.com>
2025-11-11 19:05:43 +0530

    onChange effect added for budget

--- src/create-trip/index.jsx
+++ src/create-trip/index.jsx
@@ -44,7 +44,8 @@ function CreateTrip() {
           <h2 className='text-xl my-3 font-medium'>What is Your Budget?</h2>
           <div className='grid grid-cols-3 gap-5 mt-5'>
             {SelectBudgetOptions.map((item,index)=>(
-              <div key={index} className='p-4 border rounded-lg hover:shadow cursor-pointer '>
+              <div key={index} className='p-4 border rounded-lg hover:shadow cursor-pointer '
+              onClick={()=>handleInputChange('budget',item.title)}>
                 <h2 className='text-4xl'>{item.icon}</h2>
                 <h2 className='font-bold text-lg'>{item.title}</h2>
                 <p className='text-sm text-gray-500'>{item.desc}</p>
